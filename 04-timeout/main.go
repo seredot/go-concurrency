b80308ebfdf0b01335f4a75f930fcaa380c2b373
Serdar Tahir Kabaoglu <skabaoglu@newstore.com>
2020-02-14 10:41:53 +0100

    add comments

--- 04-timeout/main.go
+++ 04-timeout/main.go
@@ -11,6 +11,8 @@ func longRunningTask(n int, c chan int) {
 	// Wait random 2 seconds
 	s := rand.Intn(2000)
 	time.Sleep(time.Duration(s) * time.Millisecond)
+
+	// Signal the waiting Goroutine that the task is complete.
 	c <- n
 }
 
@@ -21,11 +23,14 @@ func main() {
 	// Start 10 goroutines.
 	fmt.Println("Will run 10 goroutines...")
 
+	// Start 10 operations.
 	for i := 1; i <= 10; i++ {
 		wg.Add(1)
 
+		// Start a waiting Goroutine.
 		go func(n int) {
 			c := make(chan int, 1)
+			// Start an operation Goroutine.
 			go longRunningTask(n, c)
 
 			select {
